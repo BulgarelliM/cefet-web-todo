2b1d0ab5fe7ee4e9f81900cdfb7c6e61ea8dd74f
bulgarellim <bulgarelli.mariana1@gmail.com>
2021-12-19 14:29:34 -0300

    Add Atv1

--- tarefas.js
+++ tarefas.js
@@ -13,3 +13,25 @@ let tarefaY = new Tarefa('Escutar chimbinha', 'lazer', true);
 
 tarefas.push(tarefaX);
 tarefas.push(tarefaY);
+
+let containerElemento = document.querySelector('ul#lista-tarefas');
+// Diretrizes em:https://fegemo.github.io/cefet-front-end/classes/js4/#remocao-de-elementos
+containerElemento.innerHTML='';
+
+// Diretrizes em: https://fegemo.github.io/cefet-front-end/classes/js4/#16
+function insereTarefaNaPagina(obj){
+    let elementoTarefa = document.createElement('li');
+
+    elementoTarefa.innerHTML = obj.nome;
+    elementoTarefa.classList.add('item-tarefa'); // Adiciona a classe item-tarefa
+    elementoTarefa.classList.add(`categoria-${obj.categoria}`);
+
+    // Verifica se esta realizada:
+    if (obj.realizada){
+        elementoTarefa.classList.add('marcado');
+    }
+
+    containerElemento.appendChild(elementoTarefa);
+}
+
+tarefas.forEach(insereTarefaNaPagina);
